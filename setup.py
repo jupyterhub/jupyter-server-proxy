15ea53cacca048e4c9632494d3b628523b623818
ryanlovett <rylo@berkeley.edu>
2016-12-13 12:49:27 -0800

    Version 0.1.0.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="nbserverproxy",
-    version='0.0.4',
+    version='0.1.0',
     url="https://github.com/ryanlovett/nbserverproxy",
     author="Ryan Lovett",
     description="Jupyter server extension to proxy web services",
