3da7639afc824c2f35ea6879af8fcd53ab99bedd
ryanlovett <rylo@berkeley.edu>
2017-01-16 21:00:48 -0800

    Version 0.1.6.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="nbrsessionproxy",
-    version='0.1.5',
+    version='0.1.6',
     url="https://github.com/ryanlovett/nbrsessionproxy",
     author="Ryan Lovett",
     description="Jupyter extensions to proxy RStudio's rsession",
