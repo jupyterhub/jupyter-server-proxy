9b0265e51863a34cba09f6874347490f870d9d56
ryanlovett <rylo@berkeley.edu>
2017-04-13 17:49:28 -0700

    Prepare for 0.2.0.

--- setup.py
+++ setup.py
@@ -2,11 +2,13 @@ import setuptools
 
 setuptools.setup(
     name="nbrsessionproxy",
-    version='0.1.6',
+    version='0.2.0',
     url="https://github.com/ryanlovett/nbrsessionproxy",
     author="Ryan Lovett",
     description="Jupyter extensions to proxy RStudio's rsession",
     packages=setuptools.find_packages(),
-    install_requires=[ 'tornado', 'notebook' ],
+	keywords=['Jupyter'],
+	classifiers=['Framework :: Jupyter'],
+    install_requires=[ 'tornado', 'notebook', 'nbserverproxy' ],
     package_data={'nbrsessionproxy': ['static/*']},
 )
