ae8249ab9a930f87adb43672fad9f5023be93a34
ryanlovett <rylo@berkeley.edu>
2017-08-01 17:22:33 -0700

    Prepare for release on PyPI.

--- setup.py
+++ setup.py
@@ -3,9 +3,14 @@ import setuptools
 setuptools.setup(
     name="nbserverproxy",
     version='0.2.0',
-    url="https://github.com/ryanlovett/nbserverproxy",
+    url="https://github.com/jupyterhub/nbserverproxy",
     author="Ryan Lovett",
+    author_email="rylo@berkeley.edu",
+    license="BSD 3-Clause",
     description="Jupyter server extension to proxy web services",
     packages=setuptools.find_packages(),
     install_requires=[ 'tornado', 'notebook' ],
+    classifiers=[
+        'Framework :: Jupyter',
+    ]
 )
