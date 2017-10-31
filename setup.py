bb38fbedfef1686734d9f1ac8cca94751dcfd15c
yuvipanda <yuvipanda@gmail.com>
2017-10-30 21:14:10 -0700

    Bump version number

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ import setuptools
 
 setuptools.setup(
     name="nbserverproxy",
-    version='0.2.0',
+    version='0.3.0',
     url="https://github.com/jupyterhub/nbserverproxy",
     author="Ryan Lovett",
     author_email="rylo@berkeley.edu",
     license="BSD 3-Clause",
     description="Jupyter server extension to proxy web services",
     packages=setuptools.find_packages(),
-    install_requires=[ 'tornado', 'notebook' ],
+    install_requires=['notebook'],
     classifiers=[
         'Framework :: Jupyter',
     ]
