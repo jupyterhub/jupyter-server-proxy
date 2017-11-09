9fce06a904c1fc91c292b9801794290967e1119a
yuvipanda <yuvipanda@gmail.com>
2017-11-09 15:52:48 -0600

    Bump version + nbserverproxy required version
    Also remove tornado as an explicit dependency. It is
    depended on by notebook

--- setup.py
+++ setup.py
@@ -2,13 +2,16 @@ import setuptools
 
 setuptools.setup(
     name="nbrsessionproxy",
-    version='0.4.0',
+    version='0.4.1',
     url="https://github.com/jupyterhub/nbrsessionproxy",
     author="Ryan Lovett",
     description="Jupyter extension to proxy RStudio's rsession",
     packages=setuptools.find_packages(),
 	keywords=['Jupyter'],
 	classifiers=['Framework :: Jupyter'],
-    install_requires=[ 'tornado', 'notebook', 'nbserverproxy' ],
+    install_requires=[
+        'notebook',
+        'nbserverproxy >= 0.3.2'
+    ],
     package_data={'nbrsessionproxy': ['static/*']},
 )
