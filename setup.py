004c2e629287e26970d3be306a862a155b5f979e
ryanlovett <rylo@berkeley.edu>
2018-01-21 14:26:37 -0800

    Update to v0.5.0.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="nbserverproxy",
-    version='0.4',
+    version='0.5.0',
     url="https://github.com/jupyterhub/nbserverproxy",
     author="Ryan Lovett & Yuvi Panda",
     author_email="rylo@berkeley.edu",
