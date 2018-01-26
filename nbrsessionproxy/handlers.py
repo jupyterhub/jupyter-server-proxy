d35d21b2eb3e58f0fd3c5bacadd9439a12718d39
yuvipanda <yuvipanda@gmail.com>
2018-01-25 18:30:48 -0800

    Cleanup code + explicitly set USER environment variable
    - RStudio needs USER to be set, otherwise it'll push you out
      into an authentication window!
    - Since we're using rserver now rather than rsession, we do not
      need to do any R detection work.

--- nbrsessionproxy/handlers.py
+++ nbrsessionproxy/handlers.py
@@ -1,40 +1,14 @@
 import os
 import getpass
-import socket
-import subprocess
 from urllib.parse import urlunparse, urlparse
 
-from tornado import web, gen, httpclient, process, ioloop
+from tornado import web
 
 from notebook.utils import url_path_join as ujoin
 from notebook.base.handlers import IPythonHandler
 
 from nbserverproxy.handlers import SuperviseAndProxyHandler
 
-
-def detectR():
-    '''Detect R's version, R_HOME, and various other directories that rsession
-       requires.
-
-       Via rstudio's src/cpp/core/r_util/REnvironmentPosix.cpp'''
-
-    cmd = ['R', '--slave', '--vanilla', '-e',
-             'cat(paste(R.home("home"),R.home("share"),R.home("include"),R.home("doc"),getRversion(),sep=":"))']
-
-    p = subprocess.run(cmd, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    if p.returncode != 0:
-        raise Exception('Error detecting R')
-    R_HOME, R_SHARE_DIR, R_INCLUDE_DIR, R_DOC_DIR, version = \
-        p.stdout.decode().split(':')
-
-    return {
-        'R_DOC_DIR': R_DOC_DIR,
-        'R_HOME': R_HOME,
-        'R_INCLUDE_DIR': R_INCLUDE_DIR,
-        'R_SHARE_DIR': R_SHARE_DIR,
-        'RSTUDIO_DEFAULT_R_VERSION_HOME': R_HOME,
-        'RSTUDIO_DEFAULT_R_VERSION': version,
-    }
 
 class AddSlashHandler(IPythonHandler):
     """Handler for adding trailing slash to URLs that need them"""
@@ -51,16 +25,9 @@ class RSessionProxyHandler(SuperviseAndProxyHandler):
     name = 'rsession'
 
     def get_env(self):
-        env = {
-            'RSTUDIO_LIMIT_RPC_CLIENT_UID':'998',
-            'RSTUDIO_MINIMUM_USER_ID':'500',
-        }
-
-        try:
-            r_vars = detectR()
-            env.update(r_vars)
-        except:
-            raise web.HTTPError(reason='could not detect R', status_code=500)
+        env = {}
+        if 'USER' not in os.environ:
+            env['USER'] = getpass.getuser()
 
         return env
 
@@ -68,7 +35,6 @@ class RSessionProxyHandler(SuperviseAndProxyHandler):
         # rsession command. Augmented with user-identity and www-port.
         return [
             'rserver',
-            '--server-user=' + getpass.getuser(),
             '--www-port=' + str(self.port)
         ]
 
@@ -77,5 +43,3 @@ def setup_handlers(web_app):
         (ujoin(web_app.settings['base_url'], 'rstudio/(.*)'), RSessionProxyHandler, dict(state={})),
         (ujoin(web_app.settings['base_url'], 'rstudio'), AddSlashHandler)
     ])
-
-# vim: set et ts=4 sw=4:
