125fc09bef5c63bc2b4eb7e5ba658acc27103e1e
yuvipanda <yuvipanda@gmail.com>
2018-02-01 21:43:24 -0800

    Bump version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="nbserverproxy",
-    version='0.5.0',
+    version='0.5.1',
     url="https://github.com/jupyterhub/nbserverproxy",
     author="Ryan Lovett & Yuvi Panda",
     author_email="rylo@berkeley.edu",
