e47aae2f3f2e7292dc51d440ba25a8644debc957
ryanlovett <rylo@berkeley.edu>
2018-04-26 11:16:36 -0700

    Bump version to 0.8.0.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="nbserverproxy",
-    version='0.7.1',
+    version='0.8.0',
     url="https://github.com/jupyterhub/nbserverproxy",
     author="Ryan Lovett & Yuvi Panda",
     author_email="rylo@berkeley.edu",
