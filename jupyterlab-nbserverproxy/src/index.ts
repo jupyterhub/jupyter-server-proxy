227e70b124c5cc8ea95130b6aa6929985c315ebe
yuvipanda <yuvipanda@gmail.com>
2018-12-31 14:47:07 -0800

    Add JupyterLab launcher extension
    - Adds launcher icons for each registered server
    - Uses my face as icon for now, needs mechanism for packages
      to provide their own icons

---
+++ jupyterlab-nbserverproxy/src/index.ts
@@ -0,0 +1,50 @@
+import { JupyterLab, JupyterLabPlugin } from '@jupyterlab/application'; 
+import { ILauncher } from '@jupyterlab/launcher';
+import { PageConfig } from '@jupyterlab/coreutils';
+
+import '../style/index.css';
+
+
+function addLauncherEntries(serverData: any, launcher: ILauncher, app: JupyterLab) {
+    for (let entry of serverData.launcher.entries) {
+
+      let commandId = 'server-proxy:' + entry.name;
+      app.commands.addCommand(commandId, {
+        label: entry.title,
+        execute: () => {
+          let launch_url = PageConfig.getBaseUrl() + entry.name + '/';
+          window.open(launch_url, '_blank');
+        }
+      });
+      launcher.add({
+        command: commandId,
+        // This is the only way to get icon URLs in here
+        category: 'Notebook',
+        kernelIconUrl: 'http://words.yuvi.in/profile.jpg'
+      })
+    }
+}
+/**
+ * Initialization data for the jupyterlab-nbserverproxy extension.
+ */
+const extension: JupyterLabPlugin<void> = {
+  id: 'jupyterlab-nbserverproxy',
+  autoStart: true,
+  requires: [ILauncher],
+  activate: (app: JupyterLab, launcher: ILauncher) => {
+    // FIXME: What the callback hell is this
+    fetch(PageConfig.getBaseUrl() + 'server-proxy/servers-info').then(
+      response => {
+        if (!response.ok) {
+          console.log('Fetching metadata about registered failed. Make sure nbserverproxy is installed');
+          console.log(response);
+        } else {
+          response.json().then(data => addLauncherEntries(data, launcher, app))
+        }
+
+      }
+    )
+  }
+};
+
+export default extension;
