5b7e42445f3662a24c2b0d6f6c6e50b6188499d0
yuvipanda <yuvipanda@gmail.com>
2018-12-31 11:54:28 -0800

    Add entries to 'New' menu with nbextension
    - Makes it easy for all plugins to have a menu entry
      without having to write their own nbextension
    - Install & enable nbextension automatically
    - Install & enable serverextension automatically

--- nbserverproxy/__init__.py
+++ nbserverproxy/__init__.py
@@ -1,5 +1,7 @@
 from .handlers import setup_handlers, SuperviseAndProxyHandler
 from nbserverproxy.config import ServerProxy, make_proxyserver_handlers, get_entrypoint_proxy_servers
+from notebook.utils import url_path_join as ujoin
+from nbserverproxy.api import LauncherEntriesHandler, LauncherEntry
 
 # Jupyter Extension points
 def _jupyter_server_extension_paths():
@@ -9,9 +11,10 @@ def _jupyter_server_extension_paths():
 
 def _jupyter_nbextension_paths():
     return [{
-        "section": "notebook",
+        "section": "tree",
         "dest": "nbserverproxy",
-        "require": "nbserverproxy/main"
+        'src': 'static',
+        "require": "nbserverproxy/tree"
     }]
 
 
@@ -22,8 +25,20 @@ def load_jupyter_server_extension(nbapp):
     config_handlers = make_proxyserver_handlers(base_url, serverproxy.servers)
     nbapp.web_app.add_handlers('.*', config_handlers)
 
-    entrypoint_handlers = make_proxyserver_handlers(base_url, get_entrypoint_proxy_servers())
+    entrypoint_servers = get_entrypoint_proxy_servers()
+    entrypoint_handlers = make_proxyserver_handlers(base_url, entrypoint_servers)
     nbapp.web_app.add_handlers('.*', entrypoint_handlers)
 
     # Set up default handler
     setup_handlers(nbapp.web_app)
+
+    launcher_entries = []
+    for name, server in serverproxy.servers.items():
+        launcher_entries.append(LauncherEntry(name=name, title=server.get('title', name)))
+
+    for name, server in entrypoint_servers.items():
+        launcher_entries.append(LauncherEntry(name=name, title=server.get('title', name)))
+
+    nbapp.web_app.add_handlers('.*', [
+        (ujoin(base_url, 'serverproxy/launcher-entries'), LauncherEntriesHandler, {'launcher_entries': launcher_entries})
+    ])
