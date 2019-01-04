bae4e8121561bbf5954c9a04fa5d7f60c85febf8
yuvipanda <yuvipanda@gmail.com>
2019-01-04 13:53:55 -0800

    Bump version number

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from glob import glob
 
 setuptools.setup(
     name="jupyter-server-proxy",
-    version='1.0dev',
+    version='1.0beta5',
     url="https://github.com/jupyterhub/jupyter-server-proxy",
     author="Ryan Lovett & Yuvi Panda",
     author_email="rylo@berkeley.edu",
