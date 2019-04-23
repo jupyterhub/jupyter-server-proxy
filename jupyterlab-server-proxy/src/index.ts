40abba0eb96a3934d8bf4bf9ca4432dbd079fc4a
adam <athornton@gmail.com>
2019-04-23 12:28:04 -0700

    Replace Lab plugin with FrontendPlugin

--- jupyterlab-server-proxy/src/index.ts
+++ jupyterlab-server-proxy/src/index.ts
@@ -1,4 +1,4 @@
-import { JupyterLab, JupyterLabPlugin } from '@jupyterlab/application'; 
+import { JupyterLab, JupyterFrontendPlugin } from '@jupyterlab/application';
 import { ILauncher } from '@jupyterlab/launcher';
 import { PageConfig } from '@jupyterlab/coreutils';
 
@@ -6,35 +6,35 @@ import '../style/index.css';
 
 
 function addLauncherEntries(serverData: any, launcher: ILauncher, app: JupyterLab) {
-    for (let server_process of serverData.server_processes) {
+  for (let server_process of serverData.server_processes) {
 
-      if (!server_process.launcher_entry.enabled) {
-        continue;
-      }
-
-      let commandId = 'server-proxy:' + server_process.name;
-
-      app.commands.addCommand(commandId, {
-        label: server_process.launcher_entry.title,
-        execute: () => {
-          let launch_url = PageConfig.getBaseUrl() + server_process.name + '/';
-          window.open(launch_url, '_blank');
-        }
-      });
-      let command : ILauncher.IItemOptions = {
-        command: commandId,
-        category: 'Notebook'
-      };
-      if (server_process.launcher_entry.icon_url) {
-        command.kernelIconUrl =  server_process.launcher_entry.icon_url;
-      }
-      launcher.add(command);
+    if (!server_process.launcher_entry.enabled) {
+      continue;
     }
+
+    let commandId = 'server-proxy:' + server_process.name;
+
+    app.commands.addCommand(commandId, {
+      label: server_process.launcher_entry.title,
+      execute: () => {
+        let launch_url = PageConfig.getBaseUrl() + server_process.name + '/';
+        window.open(launch_url, '_blank');
+      }
+    });
+    let command: ILauncher.IItemOptions = {
+      command: commandId,
+      category: 'Notebook'
+    };
+    if (server_process.launcher_entry.icon_url) {
+      command.kernelIconUrl = server_process.launcher_entry.icon_url;
+    }
+    launcher.add(command);
+  }
 }
 /**
  * Initialization data for the jupyterlab-server-proxy extension.
  */
-const extension: JupyterLabPlugin<void> = {
+const extension: JupyterFrontendPlugin<void> = {
   id: 'jupyterlab-server-proxy',
   autoStart: true,
   requires: [ILauncher],
