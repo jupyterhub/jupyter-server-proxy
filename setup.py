4b20947b7ac8f270bf70b00a4fb0d08a3b1c6ce4
Erik Sundell <erik.i.sundell@gmail.com>
2020-04-04 19:08:55 +0200

    Release 1.3.2
    This release contains a single PR:
    - https://github.com/jupyterhub/jupyter-server-proxy/pull/183

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from glob import glob
 
 setuptools.setup(
     name="jupyter-server-proxy",
-    version='1.3.1',
+    version='1.3.2',
     url="https://github.com/jupyterhub/jupyter-server-proxy",
     author="Ryan Lovett & Yuvi Panda",
     author_email="rylo@berkeley.edu",
