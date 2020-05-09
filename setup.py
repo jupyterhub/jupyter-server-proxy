b5787024d6ebece3ce23dedf68f97b7f301634f3
ryanlovett <rylo@berkeley.edu>
2020-05-08 20:17:28 -0700

    Bump to 1.4.0.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from glob import glob
 
 setuptools.setup(
     name="jupyter-server-proxy",
-    version='1.3.2',
+    version='1.4.0',
     url="https://github.com/jupyterhub/jupyter-server-proxy",
     author="Ryan Lovett & Yuvi Panda",
     author_email="rylo@berkeley.edu",
