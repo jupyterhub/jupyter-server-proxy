1bf8212f222a41d22a7e0b2a450d46c6a1c4b9a1
Sergio Oller <sergioller@gmail.com>
2021-06-25 12:40:21 +0200

    Add the launcher_url to the server information

--- jupyter_server_proxy/api.py
+++ jupyter_server_proxy/api.py
@@ -19,7 +19,8 @@ class ServersInfoHandler(JupyterHandler):
                 'name': sp.name,
                 'launcher_entry': {
                     'enabled': sp.launcher_entry.enabled,
-                    'title': sp.launcher_entry.title
+                    'title': sp.launcher_entry.title,
+                    'launcher_url': sp.launcher_entry.launcher.url
                 },
                 'new_browser_tab' : sp.new_browser_tab
             }
