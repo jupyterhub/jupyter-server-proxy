f76842e57b939ca08ec02297e2bbf2b87f8f3db1
Erik Sundell <erik.i.sundell@gmail.com>
2021-11-30 13:23:16 +0100

    docs: update conf.py to not clutter as much

--- docs/conf.py
+++ docs/conf.py
@@ -1,32 +1,19 @@
-# -*- coding: utf-8 -*-
+# Configuration file for Sphinx to build our documentation to HTML.
 #
-# Configuration file for the Sphinx documentation builder.
+# Configuration reference: https://www.sphinx-doc.org/en/master/usage/configuration.html
 #
-# This file does only contain a selection of the most common options. For a
-# full list see the documentation:
-# http://www.sphinx-doc.org/en/master/config
 
-# -- Path setup --------------------------------------------------------------
 
-# If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
-# documentation root, use os.path.abspath to make it absolute, like shown here.
-#
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+# -- Project specific imports ------------------------------------------------
+
+import datetime
 
 
 # -- Project information -----------------------------------------------------
 
-project = u'Jupyter ServerProxy'
-copyright = u'2019, Yuvi Panda'
-author = u'Yuvi Panda'
-
-# The short X.Y version
-version = u''
-# The full version, including alpha/beta/rc tags
-release = u'1.0'
+project = "Jupyter Server Proxy"
+copyright = f"{datetime.date.today().year}, Project Jupyter Contributors"
+author = "Project Jupyter Contributors"
 
 
 # -- General configuration ---------------------------------------------------
@@ -41,16 +28,15 @@ release = u'1.0'
 extensions = [
     'sphinx.ext.intersphinx',
     'sphinx.ext.todo',
+    "sphinx_copybutton",
+    "myst_parser",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
 # The suffix(es) of source filenames.
-# You can specify multiple suffix as a list of string:
-#
-# source_suffix = ['.rst', '.md']
-source_suffix = '.rst'
+source_suffix = [".md", ".rst"]
 
 # The root toctree document.
 root_doc = master_doc = 'index'
@@ -100,87 +86,11 @@ html_static_path = ['_static']
 # html_sidebars = {}
 
 
-# -- Options for HTMLHelp output ---------------------------------------------
-
-# Output file base name for HTML help builder.
-htmlhelp_basename = 'JupyterServerProxydoc'
-
-
-# -- Options for LaTeX output ------------------------------------------------
-
-latex_elements = {
-    # The paper size ('letterpaper' or 'a4paper').
-    #
-    # 'papersize': 'letterpaper',
-
-    # The font size ('10pt', '11pt' or '12pt').
-    #
-    # 'pointsize': '10pt',
-
-    # Additional stuff for the LaTeX preamble.
-    #
-    # 'preamble': '',
-
-    # Latex figure (float) alignment
-    #
-    # 'figure_align': 'htbp',
-}
-
-# Grouping the document tree into LaTeX files. List of tuples
-# (source start file, target name, title,
-#  author, documentclass [howto, manual, or own class]).
-latex_documents = [
-    (root_doc, 'JupyterServerProxy.tex', u'Jupyter ServerProxy Documentation',
-     u'Yuvi Panda', 'manual'),
-]
-
-
-# -- Options for manual page output ------------------------------------------
-
-# One entry per manual page. List of tuples
-# (source start file, name, description, authors, manual section).
-man_pages = [
-    (root_doc, 'jupyterserverproxy', u'Jupyter ServerProxy Documentation',
-     [author], 1)
-]
-
-
-# -- Options for Texinfo output ----------------------------------------------
-
-# Grouping the document tree into Texinfo files. List of tuples
-# (source start file, target name, title, author,
-#  dir menu entry, description, category)
-texinfo_documents = [
-    (root_doc, 'JupyterServerProxy', u'Jupyter ServerProxy Documentation',
-     author, 'JupyterServerProxy', 'One line description of project.',
-     'Miscellaneous'),
-]
-
-
-# -- Options for Epub output -------------------------------------------------
-
-# Bibliographic Dublin Core info.
-epub_title = project
-
-# The unique identifier of the text. This can be a ISBN number
-# or the project homepage.
-#
-# epub_identifier = ''
-
-# A unique identification for the text.
-#
-# epub_uid = ''
-
-# A list of files that should not be packed into the epub file.
-epub_exclude_files = ['search.html']
-
-
-# -- Extension configuration -------------------------------------------------
-
 # -- Options for intersphinx extension ---------------------------------------
 
 # Example configuration for intersphinx: refer to the Python standard library.
 intersphinx_mapping = {'https://docs.python.org/': None}
+
 
 # -- Options for todo extension ----------------------------------------------
 
