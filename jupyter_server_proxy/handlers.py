4b1274eb267076185cdd6983ef01efdacb8644d8
Min RK <benjaminrk@gmail.com>
2026-09-01 16:06:36 -0700

    Add exclude_last_activity_patterns to exclude
    allows for excluding certain endpoints from activity that might keep the server alive
    even when it's really idle.
    
    and allow update_last_activity to be a callable

--- jupyter_server_proxy/handlers.py
+++ jupyter_server_proxy/handlers.py
@@ -119,6 +119,10 @@ class ProxyHandler(WebSocketHandlerMixin, JupyterHandler):
         )
         self._requested_subprotocols = None
         self.update_last_activity = kwargs.pop("update_last_activity", True)
+        self.exclude_last_activity_patterns = [
+            re.compile(pattern)
+            for pattern in kwargs.pop("exclude_last_activity_patterns", [])
+        ]
         super().__init__(*args, **kwargs)
 
     # Support/use jupyter_server config arguments allow_origin and allow_origin_pat
@@ -236,7 +240,20 @@ class ProxyHandler(WebSocketHandlerMixin, JupyterHandler):
         avoids proxied traffic being ignored by the notebook's
         internal idle-shutdown mechanism
         """
-        if self.update_last_activity:
+        update_last_activity = self.update_last_activity
+        if not update_last_activity:
+            return
+        # allow update_last_activity to be a callable
+        if callable(update_last_activity):
+            update_last_activity = update_last_activity(self)
+        else:
+            # allow excluding traffic that can still happen on 'idle',
+            # such as RStudio's `get_events` endpoint
+            path = self.request.path
+            for pattern in self.exclude_last_activity_patterns:
+                if pattern.match(path):
+                    return
+        if update_last_activity:
             self.settings["api_last_activity"] = utcnow()
 
     def _get_context_path(self, host, port):
