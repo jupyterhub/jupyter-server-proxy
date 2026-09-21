febfcb644c47e28af60a8fa00b6702dcd7f746da
Min RK <benjaminrk@gmail.com>
2026-09-21 12:53:04 -0700

    Bump to 4.7.0-0.dev

--- jupyter_server_proxy/_version.py
+++ jupyter_server_proxy/_version.py
@@ -1,4 +1,4 @@
 # __version__ should be updated using tbump, based on configuration in
 # pyproject.toml, according to instructions in RELEASE.md.
 #
-__version__ = "4.6.0"
+__version__ = "4.7.0-0.dev"
